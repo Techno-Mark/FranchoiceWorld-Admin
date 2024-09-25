083413a43509eeb5fb22383c24d2204e83b989af
Alvish6654 <alvish.pipaliya@technomark.io>
2024-09-25 17:41:19 +0530

    Register Event pop-up table column add

--- src/types/apps/registerEventListType.ts
+++ src/types/apps/registerEventListType.ts
@@ -3,6 +3,8 @@ export type registerEventType = {
   name: string;
   email: string;
   phoneNumber: string;
+  investmentCapital:string;
+  jobTitle:string;
   state?: string;
   city?: string;
   createAt?:string;
